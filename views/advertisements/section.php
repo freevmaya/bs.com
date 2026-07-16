045419fb01fd52cac80579276ec045175f59d231
Vadim Frolov <@vmaya>
2026-07-16 15:50:06 +0500

    requlary

--- views/advertisements/section.php
+++ views/advertisements/section.php
@@ -20,6 +20,32 @@ $action = ($section === Advertisement::SECTION_SELL) ? 'sell' : 'buy';
         'section' => $section,
         'action' => ['advertisements/' . $action],
     ]) ?>
+
+
+
+    <div class="sort-options">
+        <form method="get" class="form-inline">
+            <div class="form-group">
+                <select name="sort" class="form-control" onchange="this.form.submit()">
+                    <option value="-created_at" <?= Yii::$app->request->get('sort') == '-created_at' ? 'selected' : '' ?>>
+                        Новые сверху
+                    </option>
+                    <option value="created_at" <?= Yii::$app->request->get('sort') == 'created_at' ? 'selected' : '' ?>>
+                        Старые сверху
+                    </option>
+                    <option value="-price" <?= Yii::$app->request->get('sort') == '-price' ? 'selected' : '' ?>>
+                        Дорогие сверху
+                    </option>
+                    <option value="price" <?= Yii::$app->request->get('sort') == 'price' ? 'selected' : '' ?>>
+                        Дешевые сверху
+                    </option>
+                    <option value="-views_count" <?= Yii::$app->request->get('sort') == '-views_count' ? 'selected' : '' ?>>
+                        Популярные
+                    </option>
+                </select>
+            </div>
+        </form>
+    </div>
     
     <!-- Контент -->
     <div class="row">
